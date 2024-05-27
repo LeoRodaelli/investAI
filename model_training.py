7cb9ef34feb48342dda59401eeaaff886ce45d28
Pfeifer1993 <80863899+Pfeifer1993@users.noreply.github.com>
2024-05-27 19:32:53 -0300

    Add files via upload

--- model_training.py
+++ model_training.py
@@ -1,11 +1,83 @@
 import pandas as pd
+from datetime import datetime, timedelta
+import yfinance as yf
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 
+# Função para subtrair um número de dias de uma data fornecida
+def subtract_days(dt, days):
+    """
+    Subtrai um número de dias de uma data fornecida.
+    
+    Parâmetros:
+    - dt: datetime
+        A data original.
+    - days: int
+        O número de dias a serem subtraídos.
+    
+    Retorna:
+    - datetime
+        A nova data com os dias subtraídos.
+    """
+    return dt - timedelta(days=days)
+
+# Função para atualizar o arquivo CSV com novos dados
+def atualizar_csv(ticker, df_novo):
+    """
+    Atualiza um arquivo CSV com novos dados de um DataFrame fornecido.
+    
+    Parâmetros:
+    - ticker: str
+        O ticker da ação ou ativo.
+    - df_novo: DataFrame
+        O DataFrame contendo os novos dados a serem adicionados.
+    """
+    file_path = f"{ticker}.csv"
+    
+    try:
+        # Tenta carregar dados existentes do CSV
+        df_existente = pd.read_csv(file_path)
+        df_existente['Date'] = pd.to_datetime(df_existente['Date'])
+    except FileNotFoundError:
+        # Se o arquivo não existir, criar um novo DataFrame vazio
+        df_existente = pd.DataFrame()
+
+    # Garante que a coluna 'Date' esteja presente
+    if 'Date' not in df_existente.columns:
+        df_existente['Date'] = pd.NaT
+
+    # Converte as datas para timezone-naive
+    df_existente['Date'] = df_existente['Date'].dt.tz_localize(None)
+    df_novo['Date'] = df_novo['Date'].dt.tz_localize(None)
+
+    # Concatenar e remover duplicatas
+    df_atualizado = pd.concat([df_existente, df_novo])
+    df_atualizado.drop_duplicates(subset='Date', keep='last', inplace=True)
+    df_atualizado.sort_values(by='Date', inplace=True)
+
+    # Calcula a média móvel ponderada (WMA) de 14 dias
+    df_atualizado['WMA'] = df_atualizado['Close'].rolling(window=14).mean()
+
+    # Salva o DataFrame atualizado de volta no arquivo CSV
+    df_atualizado.to_csv(file_path, index=False)
+    print(f"CSV atualizado para {ticker}")
+
+# Função para carregar e preprocessar os dados
 def load_and_preprocess_data(file_path):
+    """
+    Carrega e pré-processa os dados do arquivo CSV.
+    
+    Parâmetros:
+    - file_path: str
+        O caminho do arquivo CSV.
+    
+    Retorna:
+    - data: DataFrame
+        O DataFrame pré-processado.
+    """
     data = pd.read_csv(file_path)
     data['Date'] = pd.to_datetime(data['Date'])
     data.sort_values(by='Date', inplace=True)
@@ -17,45 +89,90 @@ def load_and_preprocess_data(file_path):
     data = data.drop(columns=['Date', 'Adj Close']).dropna()
     return data
 
+# Função para calcular e mostrar as métricas
+def calcular_metricas(y_true, y_pred, modelo, ticker):
+    """
+    Calcula e exibe as métricas de avaliação do modelo.
+    
+    Parâmetros:
+    - y_true: array-like
+        Valores reais.
+    - y_pred: array-like
+        Valores previstos.
+    - modelo: str
+        Nome do modelo.
+    - ticker: str
+        O ticker da ação ou ativo.
+    """
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    print(f'Métricas para {modelo} - {ticker}:')
+    print(f'MSE: {mse}')  # Mean Squared Error (Erro Quadrático Médio)
+    print(f'MAE: {mae}')  # Mean Absolute Error (Erro Absoluto Médio)
+    print(f'R²: {r2}')  # R-Squared (Coeficiente de Determinação)
+    print('-----------------------------------')
+
+# Função para treinar o modelo
 def train_model(data, file_path):
+    """
+    Treina o modelo com os dados fornecidos e salva o modelo treinado.
+    
+    Parâmetros:
+    - data: DataFrame
+        Os dados de entrada para treinamento.
+    - file_path: str
+        O caminho do arquivo CSV contendo os dados.
+    
+    Retorna:
+    - results: list
+        Lista de strings com os melhores parâmetros e previsões para o último dia.
+    """
+    # Separar os dados em recursos (X) e alvo (y)
     X = data.drop('Close', axis=1)
     y = data['Close']
     
     # Armazenar nomes das colunas para uso pós-escalonamento
-    feature_names = X.columns
+    feature_names = X.columns.tolist()
     
+    # Padronização dos dados
     scaler = StandardScaler().fit(X)
     X_scaled = scaler.transform(X)
     
     # Converter array numpy de volta para DataFrame, preservando nomes das colunas
     X_scaled = pd.DataFrame(X_scaled, columns=feature_names)
 
+    # Modelos a serem utilizados
     models = {
         'RandomForest': RandomForestRegressor(random_state=42),
         'GradientBoosting': GradientBoostingRegressor(random_state=42)
     }
+    # Grade de parâmetros para o GridSearchCV
     param_grid = {
-       'n_estimators': [100],
-        'max_depth': [10],
-        'min_samples_split': [2],
-        'min_samples_leaf': [2]
+       'n_estimators': [100, 200, 250],  # Número de árvores no ensemble
+        'max_depth': [2, 4],  # Profundidade máxima da árvore
+        'min_samples_split': [2, 4, 6],  # Número mínimo de amostras necessárias para dividir um nó
+        'min_samples_leaf': [1, 2, 3]  # Número mínimo de amostras necessárias em um nó folha
     }
-    tscv = TimeSeriesSplit(n_splits=5)
+    # Ajustar n_splits para ser menor do que o número de amostras
+    n_splits = min(5, len(X) - 1)  # Número de divisões para validação cruzada
+
+    tscv = TimeSeriesSplit(n_splits=n_splits)
 
     results = []
     for name, model in models.items():
+        # Realiza a busca em grade com validação cruzada
         grid_search = GridSearchCV(model, param_grid, cv=tscv, scoring='neg_mean_squared_error', verbose=5, n_jobs=-1)
         grid_search.fit(X_scaled, y)
         best_model = grid_search.best_estimator_
 
-        # O GridSearchCV retrains o modelo com todo o conjunto de dados usando os melhores parâmetros encontrados.
-        # aguarda retorno do professor para modificar
-        # Salva cada modelo treinado com um nome específico para cada arquivo e modelo
+        # O GridSearchCV retreina o modelo com todo o conjunto de dados usando os melhores parâmetros encontrados.
         best_model.fit(X_scaled, y)  # O melhor modelo é retreinado explicitamente no conjunto completo
         joblib.dump(best_model, f'best_{name}_{file_path.split(".")[0]}.joblib')
 
         # Avaliação do modelo
         predictions = best_model.predict(X_scaled)
+        calcular_metricas(y, predictions, name, file_path.split(".")[0])
 
         # Previsão para o último dia disponível
         last_day_prediction = best_model.predict(X_scaled.iloc[[-1]])
@@ -65,7 +182,28 @@ def train_model(data, file_path):
     joblib.dump(scaler, f'scaler_{file_path.split(".")[0]}.joblib')
     return results
 
-files = ['MGLU3.SA.csv', 'AAPL.csv']
+# Lista de tickers das ações ou ativos a serem analisados
+tickers = ['MGLU3.SA.csv', 'AAPL.csv', 'TSLA', 'AMZO34.SA', 'BTC-USD', 'DISB34.SA', 'GOGL34.SA', 'ROXO34.SA', 'ITSA4.SA', 'BHIA3.SA']
+
+# Definir a data de hoje
+hoje = datetime.today().strftime('%Y-%m-%d')
+hoje_dt = datetime.strptime(hoje, '%Y-%m-%d')
+fim = hoje_dt - timedelta(days=1)
+
+# Definir data de início: 7 dias atrás
+inicio = subtract_days(fim, 7)
+
+for ticker in tickers:
+    # Carregar dados
+    df = yf.Ticker(ticker).history(interval='1d', start=inicio, end=fim)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+    
+    # Atualizar CSV
+    atualizar_csv(ticker, df)
+
+# Treinar o modelo após atualizar os dados
+files = ['MGLU3.SA.csv', 'AAPL.csv', 'TSLA.csv', 'AMZO34.SA.csv', 'BTC-USD.csv', 'DISB34.SA.csv', 'GOGL34.SA.csv', 'ROXO34.SA.csv', 'ITSA4.SA.csv', 'BHIA3.SA.csv']
 all_results = []
 for file_path in files:
     data = load_and_preprocess_data(file_path)
--- predict.py
+++ predict.py
@@ -1,8 +1,126 @@
 import pandas as pd
+from datetime import datetime, timedelta
+import yfinance as yf
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.preprocessing import StandardScaler
 import joblib
+from pymongo import MongoClient
 from pandas.tseries.offsets import BDay
 
+def subtract_days(dt, days):
+    """
+    Subtrai um número de dias de uma data fornecida.
+    
+    Parâmetros:
+    - dt: datetime
+        A data original.
+    - days: int
+        O número de dias a serem subtraídos.
+    
+    Retorna:
+    - datetime
+        A nova data com os dias subtraídos.
+    """
+    return dt - timedelta(days=days)
+
+def atualizar_csv(ticker, df_novo):
+    """
+    Atualiza um arquivo CSV com novos dados de um DataFrame fornecido.
+    
+    Parâmetros:
+    - ticker: str
+        O ticker da ação ou ativo.
+    - df_novo: DataFrame
+        O DataFrame contendo os novos dados a serem adicionados.
+    """
+    file_path = f"{ticker}.csv"
+    try:
+        df_existente = pd.read_csv(file_path)
+        df_existente['Date'] = pd.to_datetime(df_existente['Date'])
+    except FileNotFoundError:
+        df_existente = pd.DataFrame()
+
+    # Converte as datas para timezone-naive
+    df_existente['Date'] = df_existente['Date'].dt.tz_localize(None)
+    df_novo['Date'] = df_novo['Date'].dt.tz_localize(None)
+
+    # Concatenar e remover duplicatas
+    df_atualizado = pd.concat([df_existente, df_novo])
+    df_atualizado.drop_duplicates(subset='Date', keep='last', inplace=True)
+    df_atualizado.sort_values(by='Date', inplace=True)
+
+    # Calcula a média móvel ponderada (WMA)
+    df_atualizado['WMA'] = df_atualizado['Close'].rolling(window=14).mean()
+
+    # Salva o DataFrame atualizado de volta no arquivo CSV
+    df_atualizado.to_csv(file_path, index=False)
+    print(f"CSV atualizado para {ticker}")
+
+def conectar_mongodb():
+    """
+    Conecta ao MongoDB e retorna o objeto de banco de dados.
+    
+    Retorna:
+    - db: Database
+        O objeto de banco de dados MongoDB.
+    """
+    client = MongoClient(
+        "mongodb+srv://matheusp4:b4YEq95UskHGaC3k@invest.aju5sat.mongodb.net/?retryWrites=true&w=majority&appName=Invest",
+        tlsAllowInvalidCertificates=True
+    )
+    return client['Invest']
+
+def salvar_previsao(db, model_name, ticker, prediction, trend, overall_trend, next_date):
+    """
+    Salva a previsão no MongoDB e remove dados de datas anteriores.
+    
+    Parâmetros:
+    - db: Database
+        O objeto de banco de dados MongoDB.
+    - model_name: str
+        O nome do modelo utilizado para a previsão.
+    - ticker: str
+        O ticker da ação ou ativo.
+    - prediction: float
+        O valor previsto de fechamento.
+    - trend: str
+        A tendência prevista (Alta ou Queda).
+    - overall_trend: str
+        A tendência geral baseada em múltiplas previsões.
+    - next_date: datetime
+        A data da previsão.
+    """
+    collection = db['predictions']
+    # Remove previsões de datas anteriores para o mesmo ticker e modelo
+    collection.delete_many({'model': model_name, 'ticker': ticker})
+    prediction_data = {
+        'model': model_name,
+        'ticker': ticker,
+        'prediction': prediction,
+        'trend': trend,
+        'overall_trend': overall_trend,
+        'prediction_date': next_date.strftime('%Y-%m-%d')
+    }
+    collection.insert_one(prediction_data)
+    print(f"Previsão salva no MongoDB para {ticker} usando {model_name}.")
+
 def load_and_preprocess_data(file_path):
+    """
+    Carrega e pré-processa os dados do arquivo CSV.
+    
+    Parâmetros:
+    - file_path: str
+        O caminho do arquivo CSV.
+    
+    Retorna:
+    - data: DataFrame
+        O DataFrame pré-processado.
+    - last_date: datetime
+        A última data disponível nos dados.
+    - last_close: float
+        O último valor de fechamento disponível nos dados.
+    """
     data = pd.read_csv(file_path)
     data['Date'] = pd.to_datetime(data['Date'])
     data.sort_values(by='Date', inplace=True)
@@ -10,20 +128,31 @@ def load_and_preprocess_data(file_path):
     data['Month'] = data['Date'].dt.month
     data['Day'] = data['Date'].dt.day
     data['Weekday'] = data['Date'].dt.weekday
-    data['MA7'] = data['Close'].rolling(window=7, min_periods=1).mean().shift(1)  # Assegure a criação da coluna MA7
-    last_date = data['Date'].iloc[-1] # Salva ultima data
-    last_close = data['Close'].iloc[-1] # Salva ultimo valor de fechamento
-    data = data.drop(columns=['Date', 'Adj Close']).dropna(subset=['MA7'], how='all')  #Para garantir que nao vai ter so Na
+    data['MA7'] = data['Close'].rolling(window=7, min_periods=1).mean().shift(1)
+    data['Dividends'] = 0
+    data['Stock Splits'] = 0
+    data['WMA'] = data['Close'].rolling(window=14).mean()
+    last_date = data['Date'].iloc[-1]
+    last_close = data['Close'].iloc[-1]
+    data = data.drop(columns=['Date', 'Adj Close']).dropna(subset=['MA7'], how='all')
     return data, last_date, last_close
 
 def predict_next_day(file_path):
-    file_name = file_path.split('.')[0] # Tira o nome do arquivo sem extenão
-    data, last_date, last_close = load_and_preprocess_data(file_path) # Pre-carrega e processa os arquivos ja salvo e ajustado
+    """
+    Faz a previsão para o próximo dia útil e salva no MongoDB.
     
-    loaded_scaler = joblib.load(f'scaler_{file_name}.joblib') # Carrega o escalonador expessifico pro arquivo
+    Parâmetros:
+    - file_path: str
+        O caminho do arquivo CSV contendo os dados históricos.
+    """
+    db = conectar_mongodb()
+    file_name = file_path.split('.')[0]
+    data, last_date, last_close = load_and_preprocess_data(file_path)
     
-    next_date = last_date + BDay(1) # Calculo o proximo dia pela ultima data do arquivo
-    next_data = { # Prepara dados para prever ja com a media movel e valor da ultima linha
+    loaded_scaler = joblib.load(f'scaler_{file_name}.joblib')
+    
+    next_date = last_date + BDay(1)
+    next_data = {
         'Open': [data['Open'].iloc[-1]],
         'High': [data['High'].iloc[-1]],
         'Low': [data['Low'].iloc[-1]],
@@ -32,18 +161,57 @@ def predict_next_day(file_path):
         'Month': [next_date.month],
         'Day': [next_date.day],
         'Weekday': [next_date.weekday()],
-        'MA7': [data['MA7'].iloc[-1]]  # Garante que exista para nao trava
+        'MA7': [data['MA7'].iloc[-1]],
+        'Dividends': [0],
+        'Stock Splits': [0],
+        'WMA': [data['WMA'].iloc[-1]]
     }
     next_data_df = pd.DataFrame(next_data)
-    next_data_scaled = loaded_scaler.transform(next_data_df) # Escalona os dados com escalonador escolhido
+    feature_names = loaded_scaler.feature_names_in_
+    next_data_df = next_data_df[feature_names]
+    next_data_scaled = loaded_scaler.transform(next_data_df)
     next_data_scaled_df = pd.DataFrame(next_data_scaled, columns=next_data_df.columns)
-    models = ['RandomForest', 'GradientBoosting'] #Lista de modelo a usar
-    for model_name in models: # Aplica cada modelo pra fazer previsao do proximo dia
-        loaded_model = joblib.load(f'best_{model_name}_{file_name}.joblib')
-        next_day_prediction = loaded_model.predict(next_data_scaled_df)
-        trend = "Alta" if next_day_prediction[0] > last_close else "Queda"
-        print(f"Previsão de fechamento para {next_date.strftime('%Y-%m-%d')} usando {model_name} ({file_path}): ${next_day_prediction[0]:.2f} ({trend})")
 
-files = ['AAPL.csv', 'MGLU3.SA.csv']
+    predictions = {}
+    trends = []
+
+    models = ['RandomForest', 'GradientBoosting']
+    for model_name in models:
+        loaded_model = joblib.load(f'best_{model_name}_{file_name}.joblib')
+        next_day_prediction = loaded_model.predict(next_data_scaled_df)[0]
+        trend = "Alta" if next_day_prediction > last_close else "Queda"
+        predictions[model_name] = (next_day_prediction, trend)
+        trends.append(trend)
+        print(f"Previsão de fechamento para {next_date.strftime('%Y-%m-%d')} usando {model_name} ({file_path}): ${next_day_prediction:.2f} ({trend})")
+
+    overall_trend = "Previsão Indeterminada"
+    if trends.count("Alta") == 2:
+        overall_trend = "Grandes chances de ALTA"
+    elif trends.count("Queda") == 2:
+        overall_trend = "Grandes chances de QUEDA"
+
+    for model_name, (next_day_prediction, trend) in predictions.items():
+        salvar_previsao(db, model_name, file_name, next_day_prediction, trend, overall_trend, next_date)
+
+# Lista de tickers das ações ou ativos a serem analisados
+tickers = ['MGLU3.SA', 'AAPL', 'TSLA', 'AMZO34.SA', 'BTC-USD', 'DISB34.SA', 'GOGL34.SA', 'ROXO34.SA', 'ITSA4.SA', 'BHIA3.SA']
+
+# Definir a data de hoje
+hoje = datetime.today().strftime('%Y-%m-%d')
+hoje_dt = datetime.strptime(hoje, '%Y-%m-%d')
+fim = hoje_dt
+
+# Ajuste para garantir que pegamos dados até o dia atual
+inicio = subtract_days(fim, 10)
+
+# Atualizar os arquivos CSV com os dados mais recentes
+for ticker in tickers:
+    df = yf.Ticker(ticker).history(interval='1d', start=inicio, end=fim)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+    atualizar_csv(ticker, df)
+
+# Realizar a previsão para o próximo dia útil para cada arquivo CSV
+files = [f"{ticker}.csv" for ticker in tickers]
 for file_path in files:
     predict_next_day(file_path)
